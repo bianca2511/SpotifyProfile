8354cae53e18ad00f8fcc5a44d34fc4da2533da8
Bianca Beatrice Eni <b.b.eni@student.tue.nl>
2024-08-18 12:14:33 +0300

    authentication token works across page reloads

--- spotify-profile-demo/script.js
+++ spotify-profile-demo/script.js
@@ -2,15 +2,33 @@ const clientId = "60183ea4bf804cc48dfe6fae76168e88";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
-} else {
-    const accessToken = await getAccessToken(clientId, code);
-    const profile = await fetchProfile(accessToken);
-    console.log(profile); 
-    populateUI(profile);
+
+// store the token in the local storage so it persists after page refresh
+let storedToken = localStorage.getItem("accessToken");
+let storedExpiration = localStorage.getItem("expirationTime");
+
+// if the token is not expired, continue to use it 
+if (storedToken !== undefined && storedExpiration && Date.now() < storedExpiration) {
+  const profile = await fetchProfile(storedToken);
+  console.log(profile);
+  populateUI(profile);
+
+} else if (!code) { // if the initial code does not exist, authenticate with spotify
+
+  redirectToAuthCodeFlow(clientId);
+
+} else { // if token does not exist or is expired, ask for one
+  const tokenData = await getAccessToken(clientId, code);
+  localStorage.setItem("accessToken", tokenData.access_token);
+  localStorage.setItem("expirationTime", tokenData.expirationTime);
+
+  const profile = await fetchProfile(tokenData.access_token);
+  console.log(profile);
+  populateUI(profile);
 }
 
+///////////////////////////////////////////////
+// Redirecting to the Spotify authentication //
 
 export async function redirectToAuthCodeFlow(clientId) {
   const verifier = generateCodeVerifier(128);
@@ -30,25 +48,27 @@ export async function redirectToAuthCodeFlow(clientId) {
 }
 
 function generateCodeVerifier(length) {
-  let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
 async function generateCodeChallenge(codeVerifier) {
   const data = new TextEncoder().encode(codeVerifier);
-  const digest = await window.crypto.subtle.digest('SHA-256', data);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
   return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
 }
 
-export async function getAccessToken(clientId, code) {
+// function for getting the access token and calculating the expiration time
+async function getAccessToken(clientId, code) {
   const verifier = localStorage.getItem("verifier");
 
   const params = new URLSearchParams();
@@ -59,19 +79,25 @@ export async function getAccessToken(clientId, code) {
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: params
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params,
   });
 
-  console.log(result);
-  const { access_token } = await result.json();
-  return access_token;
+  const { access_token, expires_in } = await result.json();
+
+  const expirationTime = Date.now() + expires_in * 1000;
+
+  return { access_token, expirationTime };
 }
+
+
+///////////////////////////////////////////////////////////////////
 
 async function fetchProfile(token) {
   const result = await fetch("https://api.spotify.com/v1/me", {
-      method: "GET", headers: { Authorization: `Bearer ${token}` }
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
   });
 
   return await result.json();
@@ -80,15 +106,17 @@ async function fetchProfile(token) {
 function populateUI(profile) {
   document.getElementById("displayName").innerText = profile.display_name;
   if (profile.images[1]) {
-      const profileImage = new Image(200, 200);
-      profileImage.src = profile.images[1].url;
-      document.getElementById("avatar").appendChild(profileImage);
-      document.getElementById("imgUrl").innerText = profile.images[1].url;
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[1].url;
+    document.getElementById("avatar").appendChild(profileImage);
+    document.getElementById("imgUrl").innerText = profile.images[1].url;
   }
   document.getElementById("id").innerText = profile.id;
   document.getElementById("email").innerText = profile.email;
   document.getElementById("uri").innerText = profile.uri;
-  document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+  document
+    .getElementById("uri")
+    .setAttribute("href", profile.external_urls.spotify);
   document.getElementById("url").innerText = profile.href;
   document.getElementById("url").setAttribute("href", profile.href);
 }
